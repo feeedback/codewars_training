888fbfdf08f7a60bad941da5f586841b646084a6
A. Rzhanitsyn <risdeep@gmail.com>
2021-08-25 20:30:41 +0500

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2020: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  plugins: ['jest', 'prettier'],
+  globals: {},
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  parser: '@babel/eslint-parser',
+  rules: {
+    'import/extensions': 0,
+    'no-underscore-dangle': [2, { allow: ['__filename', '__dirname'] }],
+    'no-restricted-syntax': ['off', 'ForOfStatement'],
+    'no-await-in-loop': 'off',
+    'dot-notation': ['off'],
+    'no-console': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'prettier/prettier': ['error', { endOfLine: 'auto', printWidth: 100 }],
+  },
+};
--- 5kyu/index.test.js
+++ 5kyu/index.test.js
@@ -1,13 +1,37 @@
-/**
- * @jest-environment jsdom
- */
+import { randomInt } from 'crypto';
 
-const maxSum = require('./index.js');
+import maxSum from './index.js';
 
-describe('Basic Tests', () => {
+describe('Functionally Tests', () => {
   it('It should works for basic tests', () => {
-    expect(maxSum([1, -2, 3, 4, -5, -4, 3, 2, 1], [[1, 3], [0, 4], [6, 8]])).toEqual(6);
-    // expect(maxSum([1, -2, 3, 4, -5, -4, 3, 2, 1], [[1, 3]])).toEqual(5);
-    // expect(maxSum([1, -2, 3, 4, -5, -4, 3, 2, 1], [[1, 2], [2, 5], [1, 5], [4, 5]])).toEqual(0);
+    expect(
+      maxSum(
+        [1, -2, 3, 4, -5, -4, 3, 2, 1],
+        [
+          [1, 3],
+          [0, 4],
+          [6, 8],
+        ]
+      )
+    ).toEqual(6);
+    expect(maxSum([1, -2, 3, 4, -5, -4, 3, 2, 1], [[1, 3]])).toEqual(5);
   });
+});
+
+it('Performance Tests', () => {
+  const MAX_NUM = 1000;
+  const SIZE = 100_000;
+  const SIZE_RANGE = 100_000;
+
+  const arr = new Array(SIZE).fill(null).map(() => randomInt(MAX_NUM));
+
+  const range = new Array(SIZE_RANGE).fill(null).map(() => {
+    const start = randomInt(SIZE);
+
+    return [start, start + randomInt(SIZE - start)];
+  });
+
+  const startTime = Date.now();
+  maxSum(arr, range);
+  expect(Date.now() - startTime).toBeLessThanOrEqual(50);
 });
